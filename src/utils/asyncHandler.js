e4fc234a9ea69a7c6c815edd3fae13eeb505343f
Md Repon <repon7253@gmail.com>
2024-10-21 22:40:39 +0600

    Custom api response and error handling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+//method no: 2
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//method no: 1
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
