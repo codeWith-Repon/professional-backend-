2b3901185a3c71e601b366025ba8cae4e2350d1b
Md Repon <repon7253@gmail.com>
2024-11-25 18:25:10 +0600

    build algorithem

--- src/controllers/user.controlles.js
+++ src/controllers/user.controlles.js
@@ -1,9 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  return res.status(200).json({
-    message: "ok",
-  });
+  // get user details from frontend
+  //validation - not empty
+  // check if user already exists: username, email
+  // check for images, check for avater
+  // upload them to cloudinary, avatar
+  //crate user object - create entry in db
+  // remove password and refresh token field form response
+  //check for user creation
+  //return res
 });
 
 export { registerUser };
