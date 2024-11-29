ab078b7715dffffc18381a55960e3cab4a99f1be
Md Repon <repon7253@gmail.com>
2024-11-29 20:53:26 +0600

    create login user controller

--- src/controllers/user.controlles.js
+++ src/controllers/user.controlles.js
@@ -4,6 +4,27 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+//5.1
+const generateAccessAndRefereshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    // inject refresh token in uer
+    user.refreshToken = refreshToken;
+    // { validateBeforeSave: false } use.. becuase when user data save .. then before it check in Usermodel and when save data then it's need username or email and password.... when use this line then it save data untill comming username or email and password
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating referesh and access token"
+    );
+  }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
   //step-1: get user details from frontend
   //step-2: validation - not empty
@@ -87,4 +108,72 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  // req body -> data
+  // username or email
+  // find the uer
+  // password check
+  // access and referesh token
+  //  send cookie
+
+  // 1
+  const { email, username, password } = req.body;
+
+  //2
+  if (!username || !email) {
+    throw new ApiError(400, "username or email is required");
+  }
+
+  //3
+  const user = User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  //4
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  //5.2
+  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  //6
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged In Successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res)=> {
+  
+})
+
+export { registerUser, loginUser };
